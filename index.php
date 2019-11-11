d871ac6647c581c40ee1aec969076b483d53a5a6
daniel <mr.bain@hotmail.com>
2019-11-11 03:19:03 -0600

    cambio al index por una tonteria que jo se usaba

--- index.php
+++ index.php
@@ -2,22 +2,14 @@
 require_once "system/config.php";
 require_once "system/autoload.php";
 
-//si la session se encuentra por que exista en el servidor iniciara automaticamente la
-//sesion sino iniciara en el inicio de la web
-if(isset($_SESSION['session'])){
-	$controller = 'usuario';
-	$action = 'inicio';
-}
-else{
-	// la variable controller guarda el nombre del controlador y action guarda la acción por ejemplo registrar 
-	//si la variable controller y action son pasadas por la url desde layout.php entran en el if
-	if (isset($_GET['controller']) && isset($_GET['action'])) {
-		$controller=$_GET['controller'];
-		$action=$_GET['action'];
-	} else {
-		$controller='inicio';
-		$action='inicio';
-	}	
-}
+// la variable controller guarda el nombre del controlador y action guarda la acción por ejemplo registrar 
+//si la variable controller y action son pasadas por la url desde layout.php entran en el if
+if (isset($_GET['controller']) && isset($_GET['action'])) {
+	$controller=$_GET['controller'];
+	$action=$_GET['action'];
+} else {
+	$controller='inicio';
+	$action='inicio';
+}	
 require_once('routes.php');
 ?>
